f3957b5c140c1301d23fadcf9afb57a678057d71
erikslewis <Adelina198527@gmail.com>
2017-01-20 14:15:05 -0500

    animation

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 function openCity(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -24,4 +25,11 @@ function myMove() {
       elem.style.top = pos + 'px';
     }
   }
+}
+// Get the button, and when the user clicks on it, execute myFunction
+document.getElementById("myton").onclick = function() {myFunction()};
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
