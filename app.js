33839df5a74dc4c968f02a25a83f12ce526ae5b2
erikslewis <Adelina198527@gmail.com>
2017-06-13 14:33:22 -0400

    hire2

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ function myMove() {
     }
   }
 }
-// Get the button, and when the user clicks on it, execute myFunction
-document.getElementById("myton").onclick = function() {myFunction()};
+
 
 /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
 function myFunction() {
